d4fb8207293cc45ecb38c8fab2fe6961be1fff80
Christophe Eymard <ceymard@optilian.com>
2014-03-13 23:42:22 +0100

    added css and open sans

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var gulp = require('gulp');
 
-var clean = require('gulp-clean');
+var _clean = require('gulp-clean');
 var gutil = require('gulp-util');
 var plumber = require('gulp-plumber');
 var concat = require('gulp-concat');
@@ -22,12 +22,14 @@ var es = require('event-stream');
 var stylish = require('jshint-stylish');
 
 var config = {
+    debug: false,
     main: __dirname + '/src/apptemplates/main.jade',
     scripts: __dirname + '/src/js/**/*.js',
     templates: __dirname + '/src/js/**/*.jade',
-    styles: __dirname + '/src/**/style/*.styl',
+    styles: __dirname + '/src/style/doceo.styl',
     apptemplates: __dirname + '/src/apptemplates/**.jade',
     js_dest: __dirname + '/app/js/client',
+    css_dest: __dirname + '/app/css',
     appname: 'app'
 };
 
@@ -39,57 +41,20 @@ function logfile(msg, color) {
 }
 
 var package = JSON.parse(fs.readFileSync(__dirname + '/package.json'));
-var jade_config = {
-    debug: false,
-    package: package
-}
+config.package = package;
 
 function log_wrote() { return logfile('wrote', gutil.colors.green); }
 function log_processed() { return logfile('processed'); }
 function log_removed() { return logfile('removed', gutil.colors.red); }
+function clean() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    return gulp.src(args, {read: false})
+        .pipe(_clean())
+        .pipe(log_removed());
+}
 
 function angularTask(opts) {
     return function angularTask () {
-        opts = opts || {
-            prod: false
-        };
-
-        jade_config.debug = !opts.prod;
-
-        var sources = gulp.src(config.scripts);
-        var templates = gulp.src(config.templates);
-
-        templates = templates
-            .pipe(jade())
-            .pipe(ngtemplates());
-
-        var task = es.concat(sources, templates)
-            .pipe(ngcompile(config.appname))
-            .pipe(log_processed())
-
-        if (!opts.prod) {
-            // in dev, we want to jslint our files.
-            task = task
-                .pipe(jshint()) // we only jslint the files that are going to be included
-                .pipe(jshint.reporter(stylish))
-                // write the files independently
-                .pipe(gulp.dest(config.js_dest))
-                .pipe(log_wrote());
-        } else {
-            task = task
-                .pipe(concat('app.js'))
-                .pipe(uglify())
-                .pipe(log_wrote())
-                .pipe(gulp.dest(config.js_dest))
-        }
-
-        var main = gulp.src(config.main)
-            .pipe(jade({data: jade_config}))
-            .pipe(inject(task, {addRootSlash: false, addPrefix: '.', ignorePath: 'app'}))
-            .pipe(gulp.dest(__dirname + '/app'))
-            .pipe(log_wrote())
-
-        return main;
     }
 }
 
@@ -100,40 +65,80 @@ function styleTask(opts) {
     };
 }
 
-gulp.task('clean', function () {
-    return gulp.src([
-            config.js_dest,
-            'app/main.html'
-        ], {read: false})
-        .pipe(clean())
-        .pipe(log_removed());
+gulp.task('clean:js', function () { return clean(config.js_dest); });
+gulp.task('clean:main', function () { return clean('app/main.html'); });
+gulp.task('clean:css', function () { return clean(config.css_dest); });
+
+
+
+gulp.task('angular', ['clean:js'], function () {
+    var sources = gulp.src(config.scripts);
+    var templates = gulp.src(config.templates);
+
+    templates = templates
+        .pipe(jade())
+        .pipe(ngtemplates());
+
+    var task = es.concat(sources, templates)
+        .pipe(ngcompile(config.appname))
+        .pipe(log_processed())
+
+    if (config.debug) {
+        // in dev, we want to jslint our files.
+        task = task
+            .pipe(jshint()) // we only jslint the files that are going to be included
+            .pipe(jshint.reporter(stylish))
+            // write the files independently
+            .pipe(gulp.dest(config.js_dest))
+            .pipe(log_wrote());
+    } else {
+        task = task
+            .pipe(concat('app.js'))
+            .pipe(uglify())
+            .pipe(log_wrote())
+            .pipe(gulp.dest(config.js_dest))
+    }
+
+    return task;
 });
 
-gulp.task('angular:dev', ['clean'], angularTask({prod: false}));
-gulp.task('angular:prod', ['clean'], angularTask({prod: true}));
+gulp.task('style', ['clean:css'], function () {
+    return gulp.src(config.styles)
+        .pipe(stylus({
+            use: ['nib'],
+            set: ['compress']
+        }))
+        .pipe(gulp.dest(config.css_dest))
+        .pipe(log_wrote());
+});
 
-gulp.task('zip', ['clean', 'angular:prod'], function () {
+gulp.task('main', ['clean:main', 'angular', 'style'], function () {
+    return gulp.src(config.main)
+        .pipe(jade({data: config}))
+        .pipe(inject(gulp.src([
+                config.js_dest + '/**/*.js',
+                config.css_dest + '**/*.css'
+            ]), {addRootSlash: false, addPrefix: '.', ignorePath: 'app'}))
+        .pipe(gulp.dest(__dirname + '/app'))
+        .pipe(log_wrote())
+});
+
+gulp.task('zip', ['main', 'style', 'angular'], function () {
     return gulp.src('./app/**/*')
         .pipe(logfile('adding to zip', gutil.colors.green))
         .pipe(zip('app.nw'))
         .pipe(gulp.dest('build'));
 })
 
-gulp.task('style', function () {
-    return gulp.src(config.styles)
-    .pipe(stylus({
-        set: ['compress']
-    }));
+// config.debug = true;
+
+gulp.task('build', ['main', 'style', 'angular'])
+
+gulp.task('watch', ['style', 'angular'], function () {
+    gulp.watch(config.scripts, ['angular']);
+    gulp.watch(config.templates, ['angular']);
+    gulp.watch(config.main, ['angular']);
+    gulp.watch(config.styles, ['style']);
 });
 
-gulp.task('build', ['clean', 'angular:prod'])
-gulp.task('watch', ['clean', 'angular:dev'], function () {
-    var srcs = [config.scripts, config.style, config.templates, config.main];
-    var i = 0;
-
-    for (i = 0; i < srcs.length; i++) {
-        var src = srcs[i];
-        gulp.watch(src, ['clean', 'angular:dev']);
-    }
-});
 gulp.task('default', ['build']);
